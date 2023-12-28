f867367dca0ccaa308bf394d9a6cd015d3c9ecb6
Natarajan M <87933421+Natarajan2801@users.noreply.github.com>
2023-12-28 20:39:12 +0530

    First Commit

---
+++ src/main/java/demo/App.java
@@ -0,0 +1,36 @@
+package demo;
+import java.net.MalformedURLException;
+
+
+public class App {
+    public void getGreeting() throws InterruptedException, MalformedURLException {
+        
+        // This is to remove unnecessary warnings from your console
+        System.setProperty("java.util.logging.config.file", "logging.properties");
+        
+        TestCases tests = new TestCases(); // Initialize your test class
+
+        //TODO: call your test case functions one after other here
+        int count=0;
+        
+        if(tests.testCase01()){
+            count++;
+        }
+        if(tests.testCase02()){
+            count++;
+        }
+        if(tests.testCase03()){
+            count++;
+        }
+        if(tests.testCase04()){
+            count++;
+        }
+        System.out.println("Total no of testcases "+count);
+        //END Tests
+        tests.endTest(); // End your test by clearning connections and closing browser
+    }
+
+    public static void main(String[] args) throws InterruptedException, MalformedURLException {
+        new App().getGreeting();
+    }
+}
